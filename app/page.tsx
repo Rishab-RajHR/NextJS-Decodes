3d1491f403cafbe92cd9a18607561fb6f9785a29
Rishab Raj <rr0948555@gmail.com>
2026-09-11 10:46:12 +0530

    Static vs Dynamic Rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 
-export default function Home(){
+export default async function Home(){
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts/1',{
+        //  cache: 'no-store'
+        next: {revalidate: 10}
+    })
+    const data = await res.json()
+    console.log(data)
     return (
         <div>
-            <h1>Hello NextJS</h1>
+            <h1>Welcome to the Home Page(Static Render Page)</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
     )
 }
